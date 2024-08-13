d74e5e658cf5e75d83114da47b8a879f831fbe74
manav-01 <manavparmar696@gmail.com>
2024-08-13 18:46:32 +0530

    update 2

--- src/components/UpcomingWebinars.tsx
+++ src/components/UpcomingWebinars.tsx
@@ -5,7 +5,7 @@ import featuredWebinars from "@/data/featuredWebinars.json";
 
 function UpcomingWebinars() {
   return (
-    <div className="p-12 bg-gray-900 w-full">
+    <div className="sm:py-3 py-12 bg-gray-900 w-full">
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
         <div className="text-center">
           <h2 className="text-base text-teal-600 font-semibold tracking-wide uppercase">
